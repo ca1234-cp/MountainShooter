c57a8ec5f458dd807f40843dbdd5667bdef36620
cassio <cassiocp14@gmail.com>
2026-05-25 21:23:47 -0300

    adding mp3 son

--- code/game.py
+++ code/game.py
@@ -12,11 +12,12 @@ class Game:
         self.window = pygame.display.set_mode(size=(WINDOW_WIDTH, WINDOW_HEIGHT))
 
     def run(self, ):
+        pygame.mixer_music.load('./esset/menu.mp3')
+        pygame.mixer_music.play(-1)
         while True:
             menu = Menu(self.window)
             menu.run()
             pass
-
         # for event in pygame.event.get():
             #if event.type == pygame.QUIT:
                # pygame.quit()  # close window
